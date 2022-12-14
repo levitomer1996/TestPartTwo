b3fc8e49dfa5f90962d9f9d6a4e18369e0032372
levitomer1996 <levitomer1996@gmail.com>
2022-12-14 14:25:16 +0200

    Fixed strFormat

--- TestPartTwo/functions.c
+++ TestPartTwo/functions.c
@@ -1,21 +1,54 @@
-#include "functions.h"
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "SuperMarket.h"
+#include "functions.h"
+
+
 
 
 char* createDynStr(const char* msg)
 {
-	char* str;
-	char temp[MAX_LENGTH];
-	printf("Enter %s: ", msg);
-	fgets(str, sizeof(temp), stdin);
+	char buffer[MAX_LENGTH]; 
+	printf("%s", msg);
+	
+	getchar();//getChar cleanes the input stream from '\n'
+	fgets(buffer, sizeof(buffer), stdin);
+	char* dynamicString = malloc(strlen(buffer) + 1);
+	strcpy(dynamicString, buffer);
+	return dynamicString;
+}
 
 
 char* formatStr(const char* msg)
 {
 	char* str = createDynStr(msg);
-	char delim = "#";
+	const char delim[2] = "#";
 	char* token = strtok(str, delim);
-	printf("%s", str);
+	char* newString =NULL;
+	int pos = 0;
+	
+	while (token != NULL) {
+		printf(" %s", token);
+		token = strtok(NULL, delim);
+		pos++;
+	}
+	return newString;
 }
+
+//Swtich case in main.
+void printSuperMarketProductFunc(const SuperMarket* pSM)
+{
+	printf("The SuperMarket Products List:\n");
+	printSuperMarket(pSM);
+}
+void addProductFunc(Product* pProd)
+{
+	printf("Enter the Product you want to add:\n");
+	initProduct(pProd);
+}
+
--- TestPartTwo/functions.h
+++ TestPartTwo/functions.h
@@ -3,7 +3,14 @@
 #ifndef _FUNCTIONS_
 #define _FUNCTIONS_
 #define MAX_LENGTH 255
+#include "SuperMarket.h"
+#include "Product.h"
+
 char* createDynStr(const char* msg);
+char* formatStr(const char* msg);
 
 
+//Switch cases Main FUNC
+void printSuperMarketProductFunc(const SuperMarket* pSM);
+void addProductFunc(Product* pProd);
 #endif
